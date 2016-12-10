2cb8ffc861f5453fb148ef924769dc97842d62f0
LX2010JY <LX2010JY@163.com>
2016-12-10 12:39:57 +0800

    12-10

--- app.py
+++ app.py
@@ -2,35 +2,39 @@
 
 import requests
 from bs4 import BeautifulSoup
-import io
-import sys
-
-def draw(url,headers):
+import threading
+def order_page(url,headers):
+    '''
+        下载推荐排行榜所有小说，共300多本，测试下了一晚上，才下40多本
+    :param url:
+    :param headers:
+    :return:
+    '''
     res = requests.get(url,headers=headers)
-    res.encoding='gbk'
-    return res.text
+    res.encoding = 'gbk'
+    response = res.text
+    BsObj = BeautifulSoup(response,"html.parser")
+    list = {}
+    boxs = BsObj.findAll('div',{'class':'box'})
+    for box in boxs:
+        articals = box.findAll('li',{'class':''})
+        for artical in articals:
+            href = artical.find('a').get('href')
+            title = artical.find('a').get_text()
+            if href not in list:
+                list[href] = title
+    return list
 
-def novel(url,headers,title):
+def draw(url,headers,article_name):
+    '''
+        获取一部小说所有章，并分别下载内容
+    :param url:
+    :param headers:
+    :return:
+    '''
     res = requests.get(url,headers=headers)
     res.encoding='gbk'
     html = res.text
-    BsObj = BeautifulSoup(html,"html.parser")
-
-    content = BsObj.find('div',{'id':'content'})
-    with(open('./novel/我真是大明星.txt','a',encoding='utf-8')) as f:
-        f.write('\n'+title+'\n')
-        f.write(content.get_text())
-    print('获取'+url+'成功')
-if __name__ == '__main__':
-    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
-    url = "http://www.00ksw.com/html/15/15344/"
-    headers = {
-        'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.22 Safari/537.36 SE 2.X MetaSr 1.0',
-        'Cookie'      : 'BAIDU_SSP_lcr=http://zhannei.baidu.com/cse/search?s=10977942222484467615&q=%CE%D2%D5%E6%CA%C7%B4%F3%C3%F7%D0%C7; adwinnum=4; Hm_lvt_5501568a6d02946f5182460c1b0af1c7=1481262147,1481262197,1481262430,1481262596; Hm_lpvt_5501568a6d02946f5182460c1b0af1c7=1481262596; CNZZDATA1426290=cnzz_eid%3D783723357-1481257211-null%26ntime%3D1481257211'
-    }
-    html = draw(url,headers)
-    # with(open('./novel/index.txt','w',encoding='utf8')) as f:
-    #     f.write(html)
     BsObj = BeautifulSoup(html,"html.parser")
     a = BsObj.find('div',{'id':'list'})
     dl = a.find('dl')
@@ -39,5 +43,50 @@ if __name__ == '__main__':
         a = d.find('a')
         href = url+a.get('href')
         title = a.get_text()
-        novel(href,headers,title)
+        print('下载【'+article_name+'】中->'+title)
+        novel_dow(href,headers,title,article_name)
 
+def novel_dow(url,headers,title,article_name):
+    '''
+        下载小说每一章的内容
+    :param url:
+    :param headers:
+    :param title:
+    :return:
+    '''
+    res = requests.get(url,headers=headers)
+    res.encoding='gbk'
+    html = res.text
+    BsObj = BeautifulSoup(html,"html.parser")
+
+    content = BsObj.find('div',{'id':'content'})
+    content = str(content).replace('<br/>','\n')
+    content = content.replace('<div id="content">','')
+    content = content.replace('</div>', '')
+    content = content.replace('热门推荐:、 、 、 、 、 、 、','')
+    with(open('./novel/'+article_name+'.txt','a',encoding='utf-8')) as f:
+        f.write('\n\n'+title+'\n')
+        f.write(content)
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    # sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+    url = "http://www.00ksw.com/html/3/3133/"
+    # url = "http://www.00ksw.com/s_top/allvote.html"
+    headers = {
+        'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.22 Safari/537.36 SE 2.X MetaSr 1.0',
+    }
+
+    draw(url,headers,'斗破苍穹2')
+    # novels = order_page(url,headers)
+    # n=0
+    # for novel in novels:
+    #     t = threading.Thread(target=draw,args=(novel,headers,novels[novel]))
+    #     n = n+1
+    #     print('添加第{0}个线程'.format(n))
+    #     t.start()
